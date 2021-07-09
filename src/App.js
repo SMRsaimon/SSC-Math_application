8bd0078ea98d511ca11efe8fd0b807eb489584ea
SMRsaimon <smrsaimon960@gmail.com>
2021-07-09 07:23:29 -0700

    data update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import Img from "./resource/mathBook.jpg";
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -7,26 +6,12 @@ import Navigation from "./components/Navigation/Navigation";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import PageComponent from "./components/pagesComponent/PageComponent";
+import { useState } from "react";
+import BookStorage from "./resource/BookStorage/BookStorage";
 
 function App() {
 
-  const BookStorage={
-
-    FirstChapterPart1:[
-      
-      {
-       title: "bjhbf",
-       img:Img
-    },
-    {
-      title: "bjhbf",
-      img:Img
-   },
-  
-  ]
-
-  }
-
+   const [book, setBook]=useState(BookStorage)
 
   return (
     <>
@@ -38,91 +23,94 @@ function App() {
             <Footer />
           </Route>
           <Route path="/FirstChapter/part-1.1">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1} />
+            <PageComponent Chapter={book.FirstChapterPart1_1} />
           </Route>
           <Route path="/FirstChapter/part-1.2">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1} />
+            <PageComponent Chapter={book.FirstChapterPart1} />
           </Route>
           <Route path="/secondChapter">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1} />
+            <PageComponent Chapter={book.FirstChapterPart1} />
           </Route>
           <Route path="/ThirdChapter/part-3.1">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1} />
+            <PageComponent Chapter={book.FirstChapterPart1} />
           </Route>
           <Route path="/ThirdChapter/part-3.2">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1} />
+            <PageComponent Chapter={book.FirstChapterPart1} />
           </Route>
           <Route path="/FourthChapter">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/FiveChapter/part-5.1">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1} />
+            <PageComponent Chapter={book.FirstChapterPart1} />
           </Route>
           <Route path="/FiveChapter/part-5.2">
-            <PageComponent  Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent  Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/FiveChapter/part-5.3">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/FiveChapter/part-5.4">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1} />
+            <PageComponent Chapter={book.FirstChapterPart1} />
           </Route>
           <Route path="/FiveChapter/part-5.5">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/FiveChapter/part-5.6">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/FiveChapter/part-5.7">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/SixChapter/part-6.1">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/SixChapter/part-6.2">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/sevenChapter">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/EightChapter/part-8.1">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/EightChapter/part-8.2">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/EightChapter/part-8.3">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1} />
+            <PageComponent Chapter={book.FirstChapterPart1} />
           </Route>
           <Route path="/NineChapter/part-9.1">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
+          </Route>
+          <Route path="/NineChapter/part-9.2">
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/TenChapter/part-10.1">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/TenChapter/part-10.2">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/ElevenChapter/part-11.1">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/ElevenChapter/part-11.2">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/ElevenChapter/part-11.3">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/ElevenChapter/part-11.4">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/Twelve">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/Thirteen">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
           <Route path="/Fourteen">
-            <PageComponent Chapter={BookStorage.FirstChapterPart1}/>
+            <PageComponent Chapter={book.FirstChapterPart1}/>
           </Route>
         </Switch>
       </Router>
---
+++ src/resource/BookStorage/BookStorage.js
@@ -0,0 +1,26 @@
+import Img from "../mathBook.jpg";
+
+const BookStorage = {
+  FirstChapterPart1_1: [
+    {
+      title: "bjhbf",
+      img: Img,
+    },
+    {
+      title: "bjhbf",
+      img: Img,
+    },
+  ],
+  FirstChapterPart1_2: [
+    {
+      title: "bjhbf",
+      img: Img,
+    },
+    {
+      title: "bjhbf",
+      img: Img,
+    },
+  ],
+};
+
+export default BookStorage;
