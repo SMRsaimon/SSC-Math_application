7bebe050d595a429e886cd76c1a4c2a7b6632b85
SMRsaimon <smrsaimon960@gmail.com>
2021-07-09 03:55:18 -0700

    navigation done

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,40 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navigation.css";
 import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import NavigationData from "./NavigationData";
 
 const Navigation = () => {
-  const NavigationData = [
-    {
-      title: "First",
-      dropDown: ["1.1", "1.2"],
-    },
-    {
-      title: "Second",
-     
-    },
-    {
-      title: "Third",
-      dropDown: ["3.1", "3.2"],
-    },
-    {
-      title: "Fourth",
-    },
-    {
-        title: "Five",
-        dropDown: ["5.1", "5.2","5.3", "5.4", "5.6", "5.7"],
-      },
-  ];
+ const [navigationData, setNavigationData] = useState(NavigationData);
+
+
   return (
     <>
-      <Navbar bg="light" expand="lg">
+      <Navbar collapseOnSelect  bg="dark" variant="dark" expand="lg">
         <Navbar.Brand className="navigationLogoTitle" as={Link} to="/">SSC Math Solution </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            {NavigationData.map((iteam) =>
+            {navigationData.map((iteam) =>
               iteam.dropDown ? (
-                <NavDropdown title={iteam.title} id="basic-nav-dropdown">
+                <NavDropdown className="navigation-manue" title={iteam.title} id="basic-nav-dropdown">
                   {iteam?.dropDown?.map((x) => (
                     <NavDropdown.Item as={Link} to="/">
                       {x}
@@ -42,7 +25,7 @@ const Navigation = () => {
                   ))}
                 </NavDropdown>
               ) : (
-                <Nav.Link as={Link} to="/home">
+                <Nav.Link className="navigation-manue" as={Link} to="/home">
                   {iteam.title}
                 </Nav.Link>
               )
---
+++ src/components/Navigation/NavigationData.js
@@ -0,0 +1,60 @@
+  const NavigationData = [
+    {
+      title: "First",
+      dropDown: ["1.1", "1.2"],
+    },
+    {
+      title: "Second",
+     
+    },
+    {
+      title: "Third",
+      dropDown: ["3.1", "3.2"],
+    },
+    {
+      title: "Fourth",
+    },
+    {
+        title: "Five",
+        dropDown: ["5.1", "5.2","5.3", "5.4", "5.6", "5.7"],
+      },
+      {
+        title: "Six",
+        dropDown: ["6.1", "6.2"],
+      },
+      {
+        title: "Seven",
+       
+      },
+      {
+        title: "Eight",
+        dropDown: ["8.1", "2.2"],
+      },
+      {
+        title: "Nine",
+        dropDown: ["9.1", "9.2"],
+      },
+    
+      {
+        title: "Ten",
+        dropDown: ["10.1", "10.2"],
+      },
+      {
+        title: "Eleven",
+        dropDown: ["11.1", "11.2", "11.3", "11.4"],
+      },
+      {
+        title: "Twelve",
+       
+      },
+      {
+        title: "Thirteen",
+       
+      },
+      {
+        title: "Fourteen",
+       
+      },
+  ];
+  
+export default NavigationData;
